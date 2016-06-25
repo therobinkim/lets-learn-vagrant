97dfae2b0bc0bdce69be80e58fe18cea88a60c44
Robin Kim <therobinkim@gmail.com>
2016-06-25 00:52:40 -0500

    Organize existing bootstrap commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env bash
 
-sudo apt-get update # update list so latest version of following tools are installed
-sudo apt-get install git
-sudo apt-get install make
-sudo apt-get install curl
-sudo apt-get install mysql-server # mysql v5.5 for ubuntu v12.04 as of 3/27/16
-sudo apt-get install mongodb-server # mongodb v2.0.4 as of 4/1/16
+# update list so latest version of following tools are installed
+sudo apt-get update
 
-# mongo needs these directories to run
+# Git v1.9 as of 6/24/16 (seems outdated)
+sudo apt-get -y install git
+
+# MySQL v5.5 for ubuntu v12.04 as of 3/27/16
+# the extra stuff will skip the interactive screen,
+# which means we don't set a password
+# http://stackoverflow.com/questions/7739645/install-mysql-on-ubuntu-without-password-prompt
+sudo apt-get -y install mysql-server
+
+# MongoDB v2.0.4 as of 4/1/16
+sudo apt-get -y install mongodb-server
+# MongoDB needs these directories to run
 sudo mkdir /data
 sudo mkdir /data/db
 
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash # nvm v0.31.0 as of 3/27/16
-
-# exit shell, re-enter shell
+# nvm v0.31.0 as of 3/27/16
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+# load nvm in the future
 echo "source /home/vagrant/.nvm/nvm.sh" >> /home/vagrant/.profile
+# load nvm now
 source /home/vagrant/.profile
 
 nvm install node
